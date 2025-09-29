305c1fff3173e0cea32a82afa53cf51b2d1e2bff
SiruG <sirughimire646@gmail.com>
2025-09-29 11:59:44 +0545

    Week 5 Assignment

---
+++ LoginDTO.java
@@ -0,0 +1,22 @@
+package com.example.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LoginDTO {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100)
+    private String password;
+
+    // Getters and Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
